5a68c5e21bb773595f423dec4e7c69b6bf01b7e5
Wandu <d_wandz@yahoo.com>
2020-06-12 23:33:09 +0200

    added page creation markdown pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,49 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, Link, graphql } from "gatsby"
 
-const LinkList = props => (
-    <li style={{ display: `inline-block`, marginRight: `1em`}}>
-        <Link to={props.to}>{props.children}</Link>
-    </li>
-)
-
-export default function Layout({children}) {
-    return (
-        <div style={{margin: `3em auto`, maxWidth: 650, padding: `0 1rem`}}>
-            <header style={{marginBottom: `1.5em`}}>
-                <Link to="/" style={{textShadow: `none`, backgroundImage: `none`}}>
-                    <h3 style={{display: `inline`}}>MySweetSite</h3>
-                </Link>
-                <ul style={{listStyle: `none`, float: 'right'}}>
-                    <LinkList to="/">Home</LinkList>
-                    <LinkList to="/about/">About</LinkList>
-                    <LinkList to="/contact/">Contact</LinkList>
-                </ul>
-            </header>
-            {children}
-        </div>
-    )
+import { rhythm } from "../utils/typography"
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
 }
